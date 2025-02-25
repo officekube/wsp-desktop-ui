c7fc4c32a1d17a6c9aeea8b84fabb311dffb155c
Agboola Sherriff <rylxes@gmail.com>
2025-02-25 02:12:31 +0100

    Electron pipeline

--- public/electron.js
+++ public/electron.js
@@ -1,47 +1,53 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, protocol } = require('electron');
 const path = require('path');
+const url = require('url');
 
 async function initializeApp() {
-    const isDev = (await import('electron-is-dev')).default;
+  const isDev = (await import('electron-is-dev')).default;
+  console.log('Is Dev:', isDev);
 
-    function createWindow() {
-        // Create the browser window.
-        const win = new BrowserWindow({
-            width: 800,
-            height: 600,
-            webPreferences: {
-                nodeIntegration: true,
-                contextIsolation: false
-            },
-        });
+  function createWindow() {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false
+      },
+    });
 
-        // Load the index.html from a url
-        win.loadURL(
-            isDev
-                ? 'http://localhost:3000'
-                : `file://${path.join(__dirname, '../build/index.html')}`
-        );
+    const appPath = path.join(__dirname, '../build');
+    protocol.registerFileProtocol('file', (request, callback) => {
+      const pathname = new URL(request.url).pathname;
+      const filePath = path.join(appPath, pathname);
+      callback({ path: filePath });
+    });
 
-        // Open the DevTools in development mode.
-        if (isDev) {
-            win.webContents.openDevTools();
-        }
+    const fileUrl = url.format({
+      pathname: path.join(__dirname, '../build/index.html'),
+      protocol: 'file:',
+      slashes: true
+    });
+    win.loadURL(fileUrl).catch(err => console.error('Failed to load URL:', err));
+    win.webContents.openDevTools();
+  }
+
+  app.whenReady().then(() => {
+    console.log('App is ready');
+    createWindow();
+  }).catch(err => console.error('App failed to start:', err));
+
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit();
     }
+  });
 
-    app.whenReady().then(createWindow);
-
-    // Quit when all windows are closed.
-    app.on('window-all-closed', () => {
-        if (process.platform !== 'darwin') {
-            app.quit();
-        }
-    });
-
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow();
-        }
-    });
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
 }
 
 initializeApp().catch(console.error);
--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
     <React.StrictMode>
         <HashRouter>
+            <div>Test Render</div>
             <App />
         </HashRouter>
     </React.StrictMode>
