fec9a19f5254a7fb01f5d8098cc3c0dd06a51f32
Agboola Sherriff <rylxes@gmail.com>
2025-02-27 01:42:29 +0100

    decoupled fix

--- artifact/src/preload/index.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-  getPythonVersion: () => ipcRenderer.invoke('get-python-version'),
-  getNodeVersion: () => ipcRenderer.invoke('get-node-version'),
-});
