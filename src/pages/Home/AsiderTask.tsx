b7cbcdd516174c95fa418d97996f1cff8bb79e31
Austine <emidrubs@gmail.com>
2025-03-01 22:30:49 +0300

    Added video thumbnails with new tab opening

--- src/pages/Home/AsiderTask.tsx
+++ src/pages/Home/AsiderTask.tsx
@@ -1,8 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 
 export const AsiderTask = () => {
-  const [selectedVideo, setSelectedVideo] = useState<string | null>(null);
-
   const videos = [
     { url: "https://youtu.be/9lE0zw-cknE", title: "Overview of OfficeKube platform" },
     { url: "https://youtu.be/s-DM6bofRzo", title: "Use of a Workspace" },
@@ -22,7 +20,7 @@ export const AsiderTask = () => {
       <div className="w-full flex-none py-8">
         <div className="flex items-center justify-center">
           <span className="text-white/60 text-base font-semibold">
-            YTF Tutorial Gallery
+            YT Tutorial Gallery
           </span>
         </div>
       </div>
@@ -37,7 +35,7 @@ export const AsiderTask = () => {
             <div
               key={videoId}
               className="cursor-pointer"
-              onClick={() => setSelectedVideo(videoId)}
+              onClick={() => window.open(`https://www.youtube.com/watch?v=${videoId}`, "_blank")}
             >
               <img
                 className="w-full max-w-xs rounded-lg shadow-lg"
@@ -51,28 +49,6 @@ export const AsiderTask = () => {
           );
         })}
       </div>
-
-      {/* Video Modal */}
-      {selectedVideo && (
-        <div
-          className="fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center z-50"
-          onClick={() => setSelectedVideo(null)}
-        >
-          <div
-            className="bg-black p-2 rounded-lg shadow-lg"
-            onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside the modal
-          >
-            <iframe
-              className="w-[95vw] max-w-3xl h-[65vh] md:h-[75vh] rounded-lg"
-              src={`https://www.youtube.com/embed/${selectedVideo}`}
-              title="YouTube Video"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
-          </div>
-        </div>
-      )}
     </div>
   );
 };
